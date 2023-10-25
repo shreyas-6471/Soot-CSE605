099b8fa01b6107da5ef900958379e8a9a3eaa08c
shreyas-6471 <37994152+shreyas-6471@users.noreply.github.com>
2023-10-24 23:25:59 -0400

    Forward Flow Analysis- Intra Methods
    Updating code for Forward Flow Analysis- Intra Methods

--- sootup.examples/src/test/java/sootup/examples/basicSetup/BasicSetup.java
+++ sootup.examples/src/test/java/sootup/examples/basicSetup/BasicSetup.java
@@ -4,9 +4,6 @@ import categories.Java8Test;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import com.googlecode.dex2jar.ir.stmt.AssignStmt;
-import com.ibm.wala.cast.ir.ssa.analysis.LiveAnalysis;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import javafx.util.Pair;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -14,12 +11,7 @@ import org.junit.experimental.categories.Category;
 import sootup.core.Language;
 import sootup.core.Project;
 import sootup.core.inputlocation.AnalysisInputLocation;
-import sootup.core.jimple.basic.StmtPositionInfo;
-import sootup.core.jimple.basic.Value;
-import sootup.core.jimple.common.expr.AbstractInvokeExpr;
-import sootup.core.jimple.common.expr.JVirtualInvokeExpr;
-import sootup.core.jimple.common.ref.JArrayRef;
-import sootup.core.jimple.common.stmt.JInvokeStmt;
+import sootup.core.jimple.basic.Local;
 import sootup.core.jimple.common.stmt.Stmt;
 import sootup.core.model.SootClass;
 import sootup.core.model.SootMethod;
@@ -30,12 +22,12 @@ import sootup.java.bytecode.inputlocation.PathBasedAnalysisInputLocation;
 import sootup.java.core.JavaProject;
 import sootup.java.core.JavaSootClass;
 import sootup.java.core.JavaSootClassSource;
-import sootup.java.core.language.JavaJimple;
 import sootup.java.core.language.JavaLanguage;
 import sootup.java.sourcecode.inputlocation.JavaSourcePathAnalysisInputLocation;
 import sootup.jimple.parser.JimpleAnalysisInputLocation;
 import sootup.jimple.parser.JimpleProject;
-
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.List;
 
 /** This example illustrates how to create and use a new Soot Project. */
@@ -67,18 +59,14 @@ public class BasicSetup {
     Path pathToBinary = Paths.get("/Users/shreyassl/Documents/GitHub/Soot-CSE605/sootup.examples/src/test/resources/BasicSetup/source");
     AnalysisInputLocation<JavaSootClass> inputLocation =
         PathBasedAnalysisInputLocation.create(pathToBinary, null);
-
 //     Specify the language of the JavaProject. This is especially relevant for Multi-release jars,
 //     where classes are loaded depending on the language level of the analysis
     Language language = new JavaLanguage(8);
-
     // Create a new JavaProject based on the input location
     Project project =
         JavaProject.builder((JavaLanguage) language).addInputLocation(inputLocation).build();
-
     // Create a signature for the class we want to analyze
     ClassType classType = project.getIdentifierFactory().getClassType("HelloWorld");
-
     // Create a signature for the method we want to analyze
 //    MethodSignature methodSignature =
 //        project
@@ -91,8 +79,6 @@ public class BasicSetup {
                     .getIdentifierFactory()
                     .getMethodSignature(
                             classType, "customfn", "void", Collections.emptyList());
-
-
     // Create a view for project, which allows us to retrieve classes
     View view = project.createView();
     //System.out.println("ClassType: " + classType);
@@ -120,65 +106,131 @@ public class BasicSetup {
     SootMethod sootMethod = sootClass.getMethod(methodSignature.getSubSignature()).get();
 System.out.println(sootMethod.getBody().getStmtGraph().getEntrypoints());
 System.out.println(sootMethod.getBody().getStmts());
-
-
-    List<Stmt> stmts = sootMethod.getBody().getStmts();
+List<Stmt> stmts = sootMethod.getBody().getStmts();
     //System.out.println(sootMethod.getClass().getMethod("HelloWorld").getName());
-
     System.out.println("Printing now"+sootMethod.getBody().getStmtGraph());
     //sootMethod.getBody().getStmtGraph()
     System.out.println("Entry points are"+sootMethod.getBody().getStmtGraph().getEntrypoints());
     System.out.println("Varibales are"+sootMethod.getBody().getStmtGraph().getLabeledStmts());
     System.out.println("Locals are"+sootMethod.getBody().getLocals());
-    HashMap<Stmt, List<Object>> statementObjectsMap = new HashMap<>();
-    for(Stmt statement : stmts)
-    {
-      System.out.println("Statement is"+statement);
-      System.out.println("Defs present are"+statement.getDefs());
-      System.out.println("Hopefully right side are"+statement.getUses());
 
-      System.out.println("Uses and Defs are"+statement.getUsesAndDefs());
-      System.out.println("Statment as string is"+statement.toString());
-      System.out.println("Printing the right sides first part: " + Arrays.toString(statement.getUses().toArray()));
-//      System.out.println();
-      List<Object> util = new ArrayList<>();
-      Pair<String,List<Value>>p=new Pair<>("Defs present are",statement.getDefs());
-      util.add(p);
-      Pair<String,List<Value>>p2=new Pair<>("Uses present are",statement.getUses());
-      util.add((p2));
-      if(statement.containsInvokeExpr())
+    //Forward Flow analysis for a method level...
+    HashMap<Stmt, Set<Pair<String, Integer>>> statementInstantVals = new HashMap<>();
+      Set<Pair<String, Integer>> initset = new HashSet<>();
+      for(Local variable :sootMethod.getBody().getLocals())
       {
-        Pair<String, AbstractInvokeExpr> pair1 = new Pair<>("InvokeExprs present are", statement.getInvokeExpr());
-        util.add(pair1);
+          System.out.println("Variable is"+variable);
+          initset.add(new Pair<>(variable.toString(), Integer.MAX_VALUE));
       }
-      Pair<String,Integer>p3=new Pair<>("HashCode is",statement.hashCode());
-      util.add(p3);
-      Pair<String, Boolean>p4=new Pair<>("isBranchPresent",statement.branches());
-      util.add(p4);
-      Pair<String, StmtPositionInfo>p5=new Pair<>("Defs present are",statement.getPositionInfo());
-      util.add(statement.getPositionInfo());
-      util.add(p5);
-      Pair<String,Boolean>p6=new Pair<>("isFieldRefPresent",statement.containsFieldRef());
-      util.add(p6);
-      //statement.getDefs()
-    // statement.getUsesAndDefs()
-      //System.out.println("LHS is"+lhs);
-      if(statement.containsArrayRef())
+      Queue<Stmt> stmtQueue = new LinkedList<>();
+        for(Stmt statement:sootMethod.getBody().getStmtGraph().getEntrypoints())
+        {
+            stmtQueue.offer(statement);
+        }
+      while (!stmtQueue.isEmpty())
       {
-        Pair<String, JArrayRef> pair1 = new Pair<>("Array Refs present are", statement.getArrayRef());
-        util.add(pair1);;
+          Stmt statement = stmtQueue.poll();
+          List<Stmt>predecessors = sootMethod.getBody().getStmtGraph().predecessors(statement);
+          System.out.println(predecessors);
+
+          if (predecessors.size() == 1) {
+              System.out.println("Came here with 1 predecessor");
+              Set<Pair<String, Integer>> newset = statementInstantVals.get(predecessors.get(0));
+              Set<Pair<String,Integer>> oldset = new HashSet<>(newset);  // Deep copy of newset
+
+              transferFunction(statement, newset);
+
+              if (!newset.equals(oldset)) {
+                  for (Stmt successor : sootMethod.getBody().getStmtGraph().successors(statement)) {
+                      System.out.println("Adding the successors");
+                      stmtQueue.offer(successor);
+                  }
+              }
+              statementInstantVals.put(statement, newset);
+
+          } else if (predecessors.size() == 0) {
+              System.out.println("Came here where predec is 0");
+              Set<Pair<String, Integer>> newset = initset;
+              Set<Pair<String,Integer>> oldset = new HashSet<>(newset);  // Deep copy
+
+              System.out.println("Old set before calling transfer fn is");
+              for (Pair<String, Integer> p : oldset) {
+                  System.out.println("pair is" + p);
+              }
+
+              transferFunction(statement, newset);
+
+              System.out.println("Printing after transfer fn new set is");
+              for (Pair<String, Integer> p : newset) {
+                  System.out.println("pair is" + p);
+              }
+              System.out.println("Printing after transfer fn old set is");
+              for (Pair<String, Integer> p : oldset) {
+                  System.out.println("pair is" + p);
+              }
+
+              if (!newset.equals(oldset)) {
+                  System.out.println("Came here where we're adding successor");
+                  for (Stmt successor : sootMethod.getBody().getStmtGraph().successors(statement)) {
+                      System.out.println("Adding the successors");
+                      stmtQueue.offer(successor);
+                  }
+              }
+              statementInstantVals.put(statement, newset);
+          } else {
+              System.out.println("Came to else");
+          }
       }
-      statementObjectsMap.put(statement,util);
 
-    }
-    System.out.println("Map is formed!! printing below");
-    for (Map.Entry<Stmt, List<Object>> entry : statementObjectsMap.entrySet()) {
-      Stmt key = entry.getKey();
-      List<Object> value = entry.getValue();
-
-      System.out.println("Statement: " + key);
-      System.out.println("Associated Values: " + value);
-    }
+      HashMap<Stmt, List<Object>> statementObjectsMap = new HashMap<>();
+//    for(Stmt statement : stmts)
+//    {
+//      System.out.println("Statement is"+statement);
+//      System.out.println("Defs present are"+statement.getDefs());
+//      System.out.println("Hopefully right side are"+statement.getUses());
+//
+//      System.out.println("Uses and Defs are"+statement.getUsesAndDefs());
+//      System.out.println("Statment as string is"+statement.toString());
+//      System.out.println("Printing the right sides first part: " + Arrays.toString(statement.getUses().toArray()));
+////      System.out.println();
+//      List<Object> util = new ArrayList<>();
+//      Pair<String,List<Value>>p=new Pair<>("Defs present are",statement.getDefs());
+//      util.add(p);
+//      Pair<String,List<Value>>p2=new Pair<>("Uses present are",statement.getUses());
+//      util.add((p2));
+//      if(statement.containsInvokeExpr())
+//      {
+//        Pair<String, AbstractInvokeExpr> pair1 = new Pair<>("InvokeExprs present are", statement.getInvokeExpr());
+//        util.add(pair1);
+//      }
+//      Pair<String,Integer>p3=new Pair<>("HashCode is",statement.hashCode());
+//      util.add(p3);
+//      Pair<String, Boolean>p4=new Pair<>("isBranchPresent",statement.branches());
+//      util.add(p4);
+//      Pair<String, StmtPositionInfo>p5=new Pair<>("Defs present are",statement.getPositionInfo());
+//      util.add(statement.getPositionInfo());
+//      util.add(p5);
+//      Pair<String,Boolean>p6=new Pair<>("isFieldRefPresent",statement.containsFieldRef());
+//      util.add(p6);
+//      //statement.getDefs()
+//    // statement.getUsesAndDefs()
+//      //System.out.println("LHS is"+lhs);
+//      if(statement.containsArrayRef())
+//      {
+//        Pair<String, JArrayRef> pair1 = new Pair<>("Array Refs present are", statement.getArrayRef());
+//        util.add(pair1);;
+//      }
+//      statementObjectsMap.put(statement,util);
+//
+//    }
+//    System.out.println("Map is formed!! printing below");
+//    for (Map.Entry<Stmt, List<Object>> entry : statementObjectsMap.entrySet()) {
+//      Stmt key = entry.getKey();
+//      List<Object> value = entry.getValue();
+//
+//      System.out.println("Statement: " + key);
+//      System.out.println("Associated Values: " + value);
+//    }
 
     // Assert that Hello world print is present
 //    assertTrue(
@@ -191,4 +243,39 @@ System.out.println(sootMethod.getBody().getStmts());
 //                            .getArg(0)
 //                            .equivTo(JavaJimple.getInstance().newStringConstant("Hello World!"))));
   }
+
+    public void transferFunction(Stmt statement, Set<Pair<String, Integer>> newset)
+    {
+        if (statement.toString().contains("+") || statement.toString().contains("-") || statement.toString().contains("*") || statement.toString().contains("/"))
+        {
+            System.out.println("Encountered arithematic expression");
+        }
+        else if(statement.toString().contains("java"))
+        {
+            System.out.println("Ignoring code lines with Java");
+            return;
+        }
+        else
+        {
+            System.out.println("printing inside transfer fn" + statement.getUses().get(0).toString());
+            System.out.println("printing inside transfer fn" + statement.getDefs().get(0).toString());
+            Pair<String, Integer> toRemove = null;
+            for (Pair<String, Integer> p : newset) {
+                // To track the pair we want to remove
+                if (p.getKey().toString().equals(statement.getDefs().get(0).toString())) {
+                    System.out.println(" Found the pair to update");
+                    toRemove = p;  // Found the pair to update
+                    System.out.println("to remove is" + toRemove);
+                    break;  // Exit the loop once we find the pair
+                }
+            }
+
+            if (toRemove != null) {
+                System.out.println("Removing and adding new pair from set");
+                newset.remove(toRemove);  // Remove the old pair
+                newset.add(new Pair<>(toRemove.getKey(), Integer.parseInt(statement.getUses().get(0).toString())));  // Add the new pair with updated value
+            }
+        }
+    }
+
 }
